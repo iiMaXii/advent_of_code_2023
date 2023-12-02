067d832c27031057317addf63c0207ca0060634f
iiMaXii <max.ekstroem@gmail.com>
2023-12-02 06:57:56 +0100

    Updated year

--- day_02/main.py
+++ day_02/main.py
@@ -1,4 +1,4 @@
-"""Advent of Code 2021 - Day 2
+"""Advent of Code 2023 - Day 2
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
